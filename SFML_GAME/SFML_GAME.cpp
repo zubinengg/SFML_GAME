c7eeedd9e4c1651ea8a981980e95c933d427593d
Ravi Chhawal <ravichhawal@hotmail.com>
2025-06-11 10:38:41 +0530

    SFML Working now

--- SFML_GAME/SFML_GAME.cpp
+++ SFML_GAME/SFML_GAME.cpp
@@ -1,11 +1,40 @@
-// SFML_GAME.cpp : This file contains the 'main' function. Program execution begins and ends there.
+﻿// SFML_GAME.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
+/*
+Lines for Linker-Input (Release):
+sfml-graphics.lib
+sfml-window.lib
+sfml-audio.lib
+sfml-network.lib
+sfml-system.lib
 
-#include <iostream>
+Lines for Linker-Input (Debug):
+sfml-graphics-d.lib
+sfml-window-d.lib
+sfml-audio-d.lib
+sfml-network-d.lib
+sfml-system-d.lib
+*/
+#include <SFML/Graphics.hpp>
 
 int main()
 {
-    std::cout << "Hello World!!!!!!!!!!!!!!!!!!!!\n";
+    sf::RenderWindow window(sf::VideoMode({ 200, 200 }), "SFML works!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+
+    while (window.isOpen())
+    {
+        while (const std::optional event = window.pollEvent())
+        {
+            if (event->is<sf::Event::Closed>())
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
@@ -18,3 +47,5 @@ int main()
 //   4. Use the Error List window to view errors
 //   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
 //   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+
+
